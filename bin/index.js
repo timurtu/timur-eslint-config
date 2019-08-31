0d44107fd74abdce5161b65372d4e6bb48428e43
Timur Uzel <timurt.uzel@gmail.com>
2019-08-31 00:03:35 -0400

    finally working. adding dependencies

--- bin/index.js
+++ bin/index.js
@@ -4,22 +4,16 @@ const fs = require('fs')
 const path = require('path')
 const child_process = require('child_process')
 
-// child_process.execSync('yarn add prettier-eslint-cli -D')
+const dependencies = [
+  'prettier-eslint-cli',
+  'eslint-config-standard',
+  'eslint-plugin-import',
+  'eslint-plugin-node',
+  'eslint-plugin-promise',
+  'eslint-plugin-react'
+]
 
-// const localConfigPath = path.resolve('.eslintrc.js')
-
-// if (fs.existsSync(localConfigPath)) {
-//   fs.unlinkSync(localConfigPath)
-// }
-//
-// child_process.execSync(`touch ${localConfigPath}`)
-
-// console.log('localConfigPath', localConfigPath)
-// const configContents = fs.readFileSync('.eslintrc.js', 'utf8')
-//
-// console.log('configContents', configContents)
-//
-
+child_process.execSync(`yarn add ${dependencies.join(' ')} -D`)
 
 const prefix = child_process.execSync('npm get prefix', { encoding: 'utf8' }).trim()
 const timurConfigPath = path.join(prefix, 'lib/node_modules/timur-eslint-config/.eslintrc.js')
