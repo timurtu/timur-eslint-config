e9d51aa47ddba22c8b95f744f28b42656815755a
Timur Uzel <timurt.uzel@gmail.com>
2019-08-30 23:54:52 -0400

    patch

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const child_process = require('child_process')
 
-child_process.execSync('yarn add prettier-eslint-cli -D')
+// child_process.execSync('yarn add prettier-eslint-cli -D')
 
 // const localConfigPath = path.resolve('.eslintrc.js')
 
@@ -14,13 +14,19 @@ child_process.execSync('yarn add prettier-eslint-cli -D')
 //
 // child_process.execSync(`touch ${localConfigPath}`)
 
-const localConfigPath = process.cwd() + '/.eslintrc.js'
-console.log('localConfigPath', localConfigPath)
-const configContents = fs.readFileSync('.eslintrc.js', 'utf8')
+// const localConfigPath = process.cwd() + '/.eslintrc.js'
+// console.log('localConfigPath', localConfigPath)
+// const configContents = fs.readFileSync('.eslintrc.js', 'utf8')
+//
+// console.log('configContents', configContents)
+//
+// fs.writeFileSync(localConfigPath, configContents, {
+//   encoding: 'utf8',
+//   flag: 'w+'
+// })
 
-console.log('configContents', configContents)
+const prefix = child_process.execSync('npm get prefix', { encoding: 'utf8' }).trim()
+const timurConfigPath = path.join(prefix, 'lib/node_modules/timur-eslint-config/.eslintrc.js')
+const timurConfigContents = fs.readFileSync(timurConfigPath, 'utf8')
 
-fs.writeFileSync(localConfigPath, configContents, {
-  encoding: 'utf8',
-  flag: 'w+'
-})
+console.log(timurConfigContents)
