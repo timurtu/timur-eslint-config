528d6873765c2cdcb222cfde8bbb1f19a44df78e
Timur Uzel <timurt.uzel@gmail.com>
2019-08-30 22:06:50 -0400

    test

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+const path = require('path')
 const child_process = require('child_process')
 
 child_process.execSync('yarn add prettier-eslint-cli -D')
+
+console.log(process.cwd())
+console.log(path.resolve('.eslintrc.js'))
