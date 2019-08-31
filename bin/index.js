6bb2fcb7b72fc4a8b5bad3471544f022b6871699
Timur Uzel <timurt.uzel@gmail.com>
2019-08-30 23:13:00 -0400

    test write file

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,14 @@ const fs = require('fs')
 const path = require('path')
 const child_process = require('child_process')
 
-child_process.execSync('yarn add prettier-eslint-cli -D')
+// child_process.execSync('yarn add prettier-eslint-cli -D')
+//
+// const localConfigPath = path.resolve('.eslintrc.js')
+//
+// if (fs.existsSync(localConfigPath)) {
+//   fs.unlinkSync(localConfigPath)
+// }
+//
+// child_process.execSync(`touch ${localConfigPath}`)
 
-const localConfigPath = path.resolve('.eslintrc.js')
-
-if (fs.existsSync(localConfigPath)) {
-  fs.unlinkSync(localConfigPath)
-}
-
-child_process.execSync(`touch ${localConfigPath}`)
-fs.writeFileSync(localConfigPath, fs.readFileSync('.eslintrc.js', 'utf8'))
+fs.writeFileSync(path.resolve('.eslintrc.js'), fs.readFileSync('.eslintrc.js', 'utf8'), 'utf8')
