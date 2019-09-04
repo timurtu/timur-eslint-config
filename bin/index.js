83785d1da9394652a86f6237b848daa195125813
timurtu <timurt.uzel@gmail.com>
2019-09-04 15:37:21 -0400

    patch

--- bin/index.js
+++ bin/index.js
@@ -15,9 +15,9 @@ const dependencies = [
 
 child_process.execSync(`yarn add ${dependencies.join(' ')} -D`)
 
-const prefix = child_process.execSync('npm get prefix', { encoding: 'utf8' }).trim()
-const timurConfigPath = path.join(prefix, 'lib/node_modules/timur-eslint-config/.eslintrc.js')
-const timurConfigContents = fs.readFileSync(timurConfigPath, 'utf8')
+// const prefix = child_process.execSync('npm get prefix', { encoding: 'utf8' }).trim()
+// const timurConfigPath = path.join(prefix, 'lib/node_modules/timur-eslint-config/.eslintrc.js')
+// const timurConfigContents = fs.readFileSync(timurConfigPath, 'utf8')
 const localConfigPath = process.cwd() + '/.eslintrc.js'
 
 fs.writeFileSync(localConfigPath, require('../.eslintrc'), {
