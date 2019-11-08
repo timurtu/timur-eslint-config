d5d0d751a4273a4db290f4f56b755c8d332685de
Timur Uzel <timurt.uzel@gmail.com>
2019-11-08 14:11:28 -0500

    add indents for switch statements

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': [
       'error',
       process.platform === 'win32' ? 'windows' : 'unix',
